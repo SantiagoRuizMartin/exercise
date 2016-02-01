8e6cd01afd9f8ea2fdda8d8c1782cbaae1c85d4f
sruiz <sruiz@easysol.net>
2016-02-01 16:10:35 -0500

    Create new files

---
+++ CourseraAssignments/src/FileResource.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+/**
+ * Created by SantiagoRuiz on 01/02/2016.
+ */
+public class FileResource {
+
+    public FileResource(String Url) {
+
+    }
+
+    public  words(){
+        return null;
+    }
+}
